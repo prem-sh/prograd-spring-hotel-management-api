1754ea5f8793fec9ff8f6952f9f89a203a2e745e
Premkumar <premkumaralaguraj@gmail.com>
2022-08-03 11:23:44 +0530

    security update

--- src/main/java/com/prem/hotelmanagement/api/services/BookingServiceImpl.java
+++ src/main/java/com/prem/hotelmanagement/api/services/BookingServiceImpl.java
@@ -1,6 +1,11 @@
 package com.prem.hotelmanagement.api.services;
 
+import com.prem.hotelmanagement.api.constants.AppConstants;
 import com.prem.hotelmanagement.api.models.Booking;
+import com.prem.hotelmanagement.api.models.BookingStatus;
+import com.prem.hotelmanagement.api.models.Customer;
+import com.prem.hotelmanagement.api.models.Room;
+import com.prem.hotelmanagement.api.repository.BookingStatusRepo;
 import com.prem.hotelmanagement.api.repository.BookingsRepository;
 import com.prem.hotelmanagement.api.repository.RoomsRepository;
 import com.prem.hotelmanagement.api.repository.UserRepository;
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingServiceImpl implements BookingService{
@@ -22,6 +28,8 @@ public class BookingServiceImpl implements BookingService{
     RoomsRepository roomsRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    BookingStatusRepo bookingStatusRepo;
 
     @Override
     public ResponseEntity<String> createBooking(NewBooking bookingData) {
@@ -30,16 +38,31 @@ public class BookingServiceImpl implements BookingService{
         if(bookingData.getRoomId() == 0) return new ResponseEntity<String>("Need valid roomId", HttpStatus.NOT_ACCEPTABLE);
         if(bookingData.getInTime() == null) return new ResponseEntity<String>("Need valid inTime", HttpStatus.NOT_ACCEPTABLE);
 
+
         Date inTime = new Date();
         Date outTime = new Date();
+        try{
+            inTime = formatter.parse(bookingData.getInTime());
+            if (bookingData.getOutTime() !=null ) outTime = formatter.parse(bookingData.getOutTime());
+            else outTime = null;
+        }catch (Exception e){
+            return new ResponseEntity<>("Invalid date | date format must be <dd-MMM-yyyy HH:mm:ss>", HttpStatus.EXPECTATION_FAILED);
+        }
 
-
-
+        Optional<Room> roomObj = roomsRepository.findById(bookingData.getRoomId());
+        Optional<Customer> customerObj = userRepository.findById(bookingData.getCustomerId());
+        if(!roomObj.isPresent()) return new ResponseEntity<>("Room not found", HttpStatus.NOT_FOUND);
+        if(!customerObj.isPresent()) return new ResponseEntity<>("Customer not found", HttpStatus.NOT_FOUND);
 
         Booking booking  = new Booking();
-        booking.setRoomId(roomsRepository.getReferenceById(bookingData.getRoomId()));
-        booking.setRoomId(roomsRepository.getReferenceById(bookingData.getRoomId()));
 
+        booking.setRoomId(roomObj.get());
+        booking.setCustomerId(customerObj.get());
+        booking.setInTime(inTime);
+        booking.setOutTime(outTime);
+        booking.setBookingStatus(bookingStatusRepo.findById(AppConstants.BookingStatus.ACTIVE.id()).get());
+
+        bookingsRepository.save(booking);
         return null;
     }
 
@@ -55,7 +78,7 @@ public class BookingServiceImpl implements BookingService{
 
     @Override
     public List<Booking> getAllBooking() {
-        return null;
+        return bookingsRepository.findAll();
     }
 
     @Override
